7cdbb4ac7eb9158caa69996213dafe71f395dd99
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-05-14 09:29:11 +0300

    fitness tracker start

---
+++ fitenss-tracker/src/app/material.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule } from '@angular/material';
+
+@NgModule({
+    imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
+    exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]
+})
+export class MaterialModule{}
