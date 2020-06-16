b3ddbdebc039f002dee19666cbe6b87605aecd87
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-06-16 09:34:54 +0300

    fitness tracker restart

---
+++ fitness-tracker/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'login', component: LoginComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
