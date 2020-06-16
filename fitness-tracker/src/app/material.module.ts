cddb1c56efe4718e8e54222edaaec7192772ba55
draghicistefan <draghicistefan1984@gmail.com>
2020-06-16 14:43:30 +0300

    fitness tracker restart

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule],
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule],
+    imports: [MatFormFieldModule, MatInputModule],
+    exports: [MatFormFieldModule, MatInputModule],
 })
-export class MaterialModule{}
+export class MaterialModule {}
--- fitness-tracker/src/app/training/current-training/current-training.component.ts
+++ fitness-tracker/src/app/training/current-training/current-training.component.ts
@@ -9,7 +9,7 @@ export class CurrentTrainingComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
