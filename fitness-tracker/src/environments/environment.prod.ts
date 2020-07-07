638f76ac1d38da93fd4de070135ac7c8fc597e88
draghicistefan <draghicistefan1984@gmail.com>
2020-07-07 08:10:46 +0300

    ngrx installed

--- fitness-tracker/src/environments/environment.prod.ts
+++ fitness-tracker/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBqjJFp3eiqjdzxGgmhbxHhSH2eyH2inP4",
+    authDomain: "fitness-tracker-5021d.firebaseapp.com",
+    databaseURL: "https://fitness-tracker-5021d.firebaseio.com",
+    projectId: "fitness-tracker-5021d",
+    storageBucket: "fitness-tracker-5021d.appspot.com",
+    messagingSenderId: "218825667556",
+    appId: "1:218825667556:web:14ce6b7903695fcea20df1"
+  }
 };
