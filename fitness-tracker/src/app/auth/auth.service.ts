5e7a26d1b100dd7148ae861914e0e2f5cd674c03
draghicistefan <draghicistefan1984@gmail.com>
2020-07-07 14:42:52 +0300

    ngrx start

--- fitness-tracker/src/app/auth/auth.service.ts
+++ fitness-tracker/src/app/auth/auth.service.ts
@@ -1,4 +1,3 @@
-import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
@@ -6,6 +5,9 @@ import { Router } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { TrainingService } from '../training/training.service';
 import { UIService } from '../shared/ui.service';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../app.reducer';
+import * as UI from '../shared/ui.actions';
 
 @Injectable()
 export class AuthService
@@ -16,30 +18,37 @@ export class AuthService
     constructor(private router: Router, 
                 private angularFireAuth: AngularFireAuth,
                 private trainingService: TrainingService,
-                private uiService: UIService){}
+                private uiService: UIService,
+                private store: Store<fromRoot.State>){}
 
     registerUser(authData: AuthData)
     {
-        this.uiService.loadingStateChanged.next(true);
+        // this.uiService.loadingStateChanged.next(true);
+        this.store.dispatch(new UI.StartLoading());
         this.angularFireAuth.auth.createUserWithEmailAndPassword(authData.email, authData.password)
         .then(result=>{
-            this.uiService.loadingStateChanged.next(false);
+            // this.uiService.loadingStateChanged.next(false);
+            this.store.dispatch(new UI.StopLoading());
         })
         .catch(error=>{
-            this.uiService.loadingStateChanged.next(false);
+            // this.uiService.loadingStateChanged.next(false);
+            this.store.dispatch(new UI.StopLoading());
             this.uiService.showSnackBar(error.message, null, 3000);
         });
     }
 
     login(authData: AuthData)
     {
-        this.uiService.loadingStateChanged.next(true);
+        // this.uiService.loadingStateChanged.next(true);
+        this.store.dispatch({type: 'START_LOADING'});
         this.angularFireAuth.auth.signInWithEmailAndPassword(authData.email, authData.password)
         .then(result=>{
-            this.uiService.loadingStateChanged.next(false);
+            // this.uiService.loadingStateChanged.next(false);
+            this.store.dispatch({type: 'STOP_LOADING'});
         })
         .catch(error=>{
-            this.uiService.loadingStateChanged.next(false);
+            // this.uiService.loadingStateChanged.next(false);
+            this.store.dispatch({type: 'STOP_LOADING'});
             this.uiService.showSnackBar(error.message, null, 3000);
         });
     }
--- fitness-tracker/src/app/auth/login/login.component.ts
+++ fitness-tracker/src/app/auth/login/login.component.ts
@@ -2,37 +2,43 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { UIService } from '../../shared/ui.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../../../app/app.reducer';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit, OnDestroy
+export class LoginComponent implements OnInit
 {
   loginForm: FormGroup;
-  isLoading=false;
+  isLoading$: Observable<boolean>;
   private loadingSubs: Subscription;
 
   constructor(private authService: AuthService,
-              private uiService: UIService) { }
+              private uiService: UIService,
+              private store: Store<{ui: fromRoot.State}>) { }
 
   ngOnInit(): void 
   {
-    this.loadingSubs=this.uiService.loadingStateChanged.subscribe(isLoadingState=>{
-      this.isLoading=isLoadingState;
-    });
+    this.isLoading$=this.store.select(fromRoot.getIsLoading);
+    // this.loadingSubs=this.uiService.loadingStateChanged.subscribe(isLoadingState=>{
+    //   this.isLoading=isLoadingState;
+    // });
     this.loginForm=new FormGroup({
       email: new FormControl('', {validators:[Validators.required, Validators.email]}),
       password: new FormControl('', {validators: [Validators.required]})
     });
   }
 
-  ngOnDestroy(): void 
-  {
-    if(this.loadingSubs) this.loadingSubs.unsubscribe();
-  }
+  // ngOnDestroy(): void 
+  // {
+  //   if(this.loadingSubs) this.loadingSubs.unsubscribe();
+  // }
 
   onSubmit()
   {
